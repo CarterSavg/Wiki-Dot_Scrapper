d15e20ceca3eca94924e13aad311bdcdb83b6ca3
CarterSavg <cartersavage15@gmail.com>
2024-12-24 11:43:48 -0500

    lists all spells

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+def scrape_classes():
+    '''Scrapes the classes from dnd5e wikidot using the classes.txt file as the extensions for each class'''
+    file = open("classes.txt", "r")
+    extensions = file.read().splitlines()
 
-file = open("classes.txt", "r")
-extensions = file.read().splitlines()
+    for extension in extensions:
+        if extension == "fighter":
+            url = f'https://dnd5e.wikidot.com/{extension}'
+            page = requests.get(url, timeout=10)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            print(soup.text)
 
-# url = 'https://dnd5e.wikidot.com/'
-# page = requests.get(url, timeout=10)
-# soup = BeautifulSoup(page.content, 'html.parser')
-# classes = soup.select("h1 span a")
-# for name in classes:
-#     print(name.text.replace(' ', '-').lower())
+def scrape_spells():
+    url = 'https://dnd5e.wikidot.com/spells'
+    page = requests.get(url, timeout=10)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    for level in range(0,10):    
+        spells = soup.select(f"#wiki-tab-0-{level} tr")
+        print(spells)
+        for name in spells:
+            print(name.text.replace(' ', '-').lower())
+
+scrape_spells()
