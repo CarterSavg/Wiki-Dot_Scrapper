6667dc4c667280ddae90689a6158fb44b51ca996
CarterSavg <cartersavage15@gmail.com>
2024-12-24 11:02:33 -0500

    read in from classes.txt

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://dnd5e.wikidot.com/'
-page = requests.get(url, timeout=10)
-soup = BeautifulSoup(page.content, 'html.parser')
-classes = soup.select("h1 span a")
-for name in classes:
-    print(name.text.replace(' ', '-').lower())
+
+file = open("classes.txt", "r")
+extensions = file.read().splitlines()
+
+# url = 'https://dnd5e.wikidot.com/'
+# page = requests.get(url, timeout=10)
+# soup = BeautifulSoup(page.content, 'html.parser')
+# classes = soup.select("h1 span a")
+# for name in classes:
+#     print(name.text.replace(' ', '-').lower())
