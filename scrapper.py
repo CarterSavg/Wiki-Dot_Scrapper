f5a5eb4e9de575e4426a22c3af9e0f99c43eef20
CarterSavg <cartersavage15@gmail.com>
2024-12-30 13:26:22 -0500

    updated scrapping architecture

--- scrapper.py
+++ scrapper.py
@@ -13,7 +13,17 @@ def scrape_classes():
             soup = BeautifulSoup(page.content, 'html.parser')
             print(soup.text)
 
-def scrape_spells():
+def scrape_spell(links):
+    '''Scrapes the individual spell information and puts it into an object before returning the list of objects'''
+    for level in links:
+        for spell in level:
+            url = f'https://dnd5e.wikidot.com{spell}'
+            page = requests.get(url, timeout=10)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            print(soup)
+
+def scrape_spell_links():
+    '''gets all of the hrefs from the tables at the following address: https://dnd5e.wikidot.com/spells and returns it as a list of lists ordered by spell level'''
     url = 'https://dnd5e.wikidot.com/spells'
     page = requests.get(url, timeout=10)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -22,9 +32,12 @@ def scrape_spells():
         links = soup.select(f"#wiki-tab-0-{level} tr a")
         hrefs = [link['href'] for link in links]
         hrefs_arr.append(hrefs)
-    print(hrefs_arr)
-        # print(f'\nLevel {level}:')
-        # for name in spells:
-        #     print(name.text.lower().replace('/', ' ').replace(':', '').replace(' ', '-').replace("(hb)", ''))
 
-scrape_spells()
+
+def scrape_spells_brain():
+    '''Scrapes all of the spells from https://dnd5e.wikidot.com/spells and returns them as a list of objects'''
+
+    links = scrape_spell_links()
+    scrape_spell(links)
+    
+scrape_spells_brain()
