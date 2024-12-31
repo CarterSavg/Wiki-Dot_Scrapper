331a1913677504787e62bd75c0c36b090a5e9de1
CarterSavg <cartersavage15@gmail.com>
2024-12-30 23:05:31 -0500

    scrapes spell description

--- scrapper.py
+++ scrapper.py
@@ -14,10 +14,13 @@ def scrape_classes():
             print(soup.text)
 
 def scrape_spell(links):
+    #TODO:
+    # create class and constructor
+    # get school name and level, different for cantrips and leveled spells
     '''Scrapes the individual spell information and puts it into an object before returning the list of objects'''
     # for level in links:
     #     for spell in level:
-    url = f'https://dnd5e.wikidot.com{links[2][1]}'
+    url = f'https://dnd5e.wikidot.com{links[3][0]}'
     page = requests.get(url, timeout=10)
     soup = BeautifulSoup(page.content, 'html.parser')
     content = soup.select("#page-content p")
@@ -28,6 +31,7 @@ def scrape_spell(links):
     range_ = soup.find('strong', string='Range:').next_sibling.strip()
     components = soup.find('strong', string='Components:').next_sibling.strip()
     duration = soup.find('strong', string='Duration:').next_sibling.strip()
+    desc = ""
     higher_levels = None
     try:
         higher_levels = soup.find('strong', string='At Higher Levels.').next_sibling.strip()
@@ -40,6 +44,13 @@ def scrape_spell(links):
     print(f"users {content[-1].text.replace(" ", "").split(".")[1].split(",")}")
     if higher_levels:
         print(higher_levels)
+        for part in content[3:-2]:
+            desc += part.text
+        print(desc)
+    else:
+        for part in content[3:-1]:
+            desc += part.text
+        print(desc)
 
 def scrape_spell_links():
     '''gets all of the hrefs from the tables at the following address: https://dnd5e.wikidot.com/spells and returns it as a list of lists ordered by spell level'''
