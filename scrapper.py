9e2db5fa07e1001150caa37866d92d6cc7ca50d1
CarterSavg <cartersavage15@gmail.com>
2025-01-01 17:43:54 -0500

    scrapes all spells

--- scrapper.py
+++ scrapper.py
@@ -18,43 +18,54 @@ def scrape_spell(links):
     # create class and constructor
     # Itterate through all spells
     '''Scrapes the individual spell information and puts it into an object before returning the list of objects'''
-    # for level in links:
-    #     for spell in level:
-    level = 0
-    url = f'https://dnd5e.wikidot.com{links[0][0]}'
-    page = requests.get(url, timeout=10)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    content = soup.select("#page-content p")
-    title = soup.select(".page-title")
-    print(title[0].text)
-    if level > 0:
-        print(f"school {content[1].text.split(" ")[-1].capitalize()}")
-    else:
-        print(f"school {content[1].text.split(" ")[0].capitalize()}")
-    casting_time = soup.find('strong', string='Casting Time:').next_sibling.strip()
-    range_ = soup.find('strong', string='Range:').next_sibling.strip()
-    components = soup.find('strong', string='Components:').next_sibling.strip()
-    duration = soup.find('strong', string='Duration:').next_sibling.strip()
-    desc = ""
-    higher_levels = None
-    try:
-        higher_levels = soup.find('strong', string='At Higher Levels.').next_sibling.strip()
-    except:
-        pass
-    print("Casting Time:", casting_time)
-    print("Range:", range_)
-    print("Components:", components)
-    print("Duration:", duration)
-    print(f"users {content[-1].text.replace(" ", "").split(".")[1].split(",")}")
-    if higher_levels:
-        print(higher_levels)
-        for part in content[3:-2]:
-            desc += part.text
-        print(desc)
-    else:
-        for part in content[3:-1]:
-            desc += part.text
-        print(desc)
+    for level_num, level in enumerate(links):
+        for spell in level:
+    # spell = "/spell:acid-splash"
+    # level_num = 0
+            url = f'https://dnd5e.wikidot.com{spell}'
+            page = requests.get(url, timeout=10)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            content = soup.select("#page-content p")
+            title = soup.select(".page-title")
+            print(title[0].text)
+            if level_num > 0:
+                print(f"school {content[1].text.split(" ")[-1].capitalize()}")
+            else:
+                print(f"school {content[1].text.split(" ")[0].capitalize()}")
+            casting_time = soup.find('strong', string='Casting Time:').next_sibling.strip()
+            range_ = soup.find('strong', string='Range:').next_sibling.strip()
+            components = soup.find('strong', string='Components:').next_sibling.strip()
+            duration = soup.find('strong', string='Duration:').next_sibling.strip()
+            try:
+                users = content[-1].text.replace(" ", "").split(".")[1].split(",") #getting all users after the . in "Spell List."
+            except IndexError: #Spell List has a : instead of a . 
+                users = content[-1].text.replace(" ", "").split(":")[1].split(",") #getting all users after the : in "Spell List:"  
+            desc = ""
+            higher_levels = None
+            try:
+                higher_levels = soup.find('strong', string=('At Higher Levels.', 'At Higher Levels:')).next_sibling.strip()
+            except:
+                pass
+            print("Casting Time:", casting_time)
+            print("Range:", range_)
+            print("Components:", components)
+            print("Duration:", duration)
+            print(f"users {users}")
+            if higher_levels:
+                print(higher_levels)
+                for part in content[3:-2]:
+                    desc += part.text
+                extra = soup.select("#page-content ul")
+                for part in extra:
+                    desc += part.text
+                print(desc)
+            else:
+                for part in content[3:-1]:
+                    desc += part.text
+                extra = soup.select("#page-content ul")
+                for part in extra:
+                    desc += part.text
+                print(desc)
 
 def scrape_spell_links():
     '''gets all of the hrefs from the tables at the following address: https://dnd5e.wikidot.com/spells and returns it as a list of lists ordered by spell level'''
