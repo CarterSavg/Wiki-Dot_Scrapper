5c4628d475dfc91ede729a7daaa999e293255685
CarterSavg <cartersavage15@gmail.com>
2025-07-01 11:07:31 -0400

    Update api.py
    return dict instead of default

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
-import psycopg2
+import psycopg2.extras
 import os
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def hello():
     load_dotenv()
     DATABASE_URL = os.getenv("DATABASE_URL")
     conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cursor.execute('''SELECT * FROM spells''')
     data = cursor.fetchall()
     cursor.close()
