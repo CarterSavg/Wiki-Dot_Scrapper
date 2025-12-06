4345f40986f8e9d7b5cad734a30a14155e726016
CarterSavg <cartersavage15@gmail.com>
2025-12-06 17:17:17 -0500

    get data

--- webpage/index.js
+++ webpage/index.js
@@ -4,7 +4,19 @@ import express from "express";
 const app = express()
 const port = 3000
 
-
+app.get('/', async (req, res) => {
+    try{
+        const response = await axios.get('http://127.0.0.1:5000');
+        const result = response.data;
+        console.log(result)
+        // res.render('index.ejs', {secret: result.secret, user: result.username})
+    } catch (error) {
+        // res.render("index.ejs", {
+        // error: error.message,
+        // });
+    }
+    
+})
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
