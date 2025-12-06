81d5611de89400e851e039771ca652e016ef7f4b
CarterSavg <cartersavage15@gmail.com>
2025-12-06 17:36:13 -0500

    render all spells
    display all spell names

--- webpage/index.js
+++ webpage/index.js
@@ -4,12 +4,14 @@ import express from "express";
 const app = express()
 const port = 3000
 
+app.use(express.static("public"));
+
 app.get('/', async (req, res) => {
     try{
         const response = await axios.get('http://127.0.0.1:5000');
         const result = response.data;
         console.log(result)
-        // res.render('index.ejs', {secret: result.secret, user: result.username})
+        res.render('index.ejs', {spells: result})
     } catch (error) {
         // res.render("index.ejs", {
         // error: error.message,
