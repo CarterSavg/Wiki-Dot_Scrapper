de1ec59590211f0695cb561f3abe145c4c912f5e
CarterSavg <cartersavage15@gmail.com>
2026-02-07 15:56:07 -0500

    basic data passthrough to frontend

--- webpage/Backend/index.js
+++ webpage/Backend/index.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 import express from "express";
-
+import cors from "cors"
 const app = express()
 const port = 3000
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(cors());
 
 app.get('/', async (req, res) => {
     try{
         const response = await axios.get('http://127.0.0.1:5000');
         const result = response.data;
         console.log(result)
-        res.render('index.ejs', {spells: result})
+        // res.render('index.ejs', {spells: result})
+        res.json(result)
     } catch (error) {
         res.render("index.ejs", {
         error: error.message,
