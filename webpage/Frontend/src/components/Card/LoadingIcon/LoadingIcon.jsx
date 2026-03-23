9ea845c6ea2f15a3b931ac090e088e232bc1db9e
CarterSavg <cartersavage15@gmail.com>
2026-03-23 19:42:58 -0400

    Created basic loading icon

---
+++ webpage/Frontend/src/components/Card/LoadingIcon/LoadingIcon.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './LoadingIcon.css';
+import loadingIcon from '../../../assets/Book.webm';
+
+
+function LoadingIcon() {
+  return (
+    <div class="loading-book">
+        <video className ="loading-video" width="300" height="300" autoPlay loop muted playsInline>
+            <source src={loadingIcon} type="video/webm" />
+      </video>
+    </div>
+  )
+}
+
+export default LoadingIcon;
