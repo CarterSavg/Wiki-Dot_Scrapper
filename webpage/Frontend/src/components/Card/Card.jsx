ea6063b3808bf9eef0e2a88d8d6c1e84723316bf
CarterSavg <cartersavage15@gmail.com>
2026-03-27 20:33:56 -0400

    added expanding cards

--- webpage/Frontend/src/components/Card/Card.jsx
+++ webpage/Frontend/src/components/Card/Card.jsx
@@ -2,27 +2,33 @@ import { useState } from 'react'
 import './Card.css'
 
 function Card(props) {
+    const [expanded, setExpanded] = useState(false)
+
+    function toggleExpand() {
+        setExpanded(!expanded)
+    }
+
     return (
-        <div class="spell-card">
+        <div onClick={toggleExpand}class="spell-card">
             <p class="spell-name"> {props.name}</p>
             <p class="casting-time">Casting Time: {props.castingTime} </p>
             <p class="spell-distance">Distance: {props.distance} </p>
             <p class="spell-school"> {props.school} </p>
             <p class="spell-level"> {props.level === 0 ? "Cantrip" : "Level: " + props.level}</p>
-            <p class="spell-description">{props.description} </p>
-            <p class="spell-higher"> {props.higherLevel} </p>
+            {expanded && <p class="spell-description">{props.description} </p>}
+            {expanded && <p class="spell-higher"> {props.higherLevel} </p>}
             <p class="spell-component">
                 {props.verbal ? "V" : ""}
                 {props.somatic ? "S" : ""}
                 {props.material ? "M" : ""}
             </p>
-            <p class="material-desc"> {props.material ? "Material: " + props.materialDesc : null}</p>
-            <ul>
+            {expanded && <p class="material-desc"> {props.material ? "Material: " + props.materialDesc : null}</p>}
+            {expanded && <ul>
                 {props.users.map((user, index) => (
                     <li key={index}>{user}</li>
                 ))}
 
-            </ul>
+            </ul>}
         </div>
     )
 }
