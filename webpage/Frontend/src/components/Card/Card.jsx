8e2744a3d8c24300254451f68c8c53796d468e9e
CarterSavg <cartersavage15@gmail.com>
2026-04-05 12:16:50 -0400

    cleanup class errors

--- webpage/Frontend/src/components/Card/Card.jsx
+++ webpage/Frontend/src/components/Card/Card.jsx
@@ -9,20 +9,20 @@ function Card(props) {
     }
 
     return (
-        <div onClick={toggleExpand}class="spell-card">
-            <p class="spell-name"> {props.name}</p>
-            <p class="casting-time">Casting Time: {props.castingTime} </p>
-            <p class="spell-distance">Distance: {props.distance} </p>
-            <p class="spell-school"> {props.school} </p>
-            <p class="spell-level"> {props.level === 0 ? "Cantrip" : "Level: " + props.level}</p>
-            {expanded && <p class="spell-description">{props.description} </p>}
-            {expanded && <p class="spell-higher"> {props.higherLevel} </p>}
-            <p class="spell-component">
-                <p class="verbal-tag">{props.verbal ? "V" : ""}</p>
-                <p class="somatic-tag">{props.somatic ? "S" : ""}</p>
-                <p class="material-tag">{props.material ? "M" : ""}</p>
-            </p>
-            {expanded && <p class="material-desc"> {props.material ? "Material: " + props.materialDesc : null}</p>}
+        <div onClick={toggleExpand}className="spell-card">
+            <p className="spell-name"> {props.name}</p>
+            <p className="casting-time">Casting Time: {props.castingTime} </p>
+            <p className="spell-distance">Distance: {props.distance} </p>
+            <p className="spell-school"> {props.school} </p>
+            <p className="spell-level"> {props.level === 0 ? "Cantrip" : "Level: " + props.level}</p>
+            {expanded && <p className="spell-description">{props.description} </p>}
+            {expanded && <p className="spell-higher"> {props.higherLevel} </p>}
+            <ul className="spell-component">
+                <li className="verbal-tag">{props.verbal ? "V" : ""}</li>
+                <li className="somatic-tag">{props.somatic ? "S" : ""}</li>
+                <li className="material-tag">{props.material ? "M" : ""}</li>
+            </ul>
+            {expanded && <p className="material-desc"> {props.material ? "Material: " + props.materialDesc : null}</p>}
             {expanded && <ul>
                 {props.users.map((user, index) => (
                     <li key={index}>{user}</li>
--- webpage/Frontend/src/components/LoadingIcon/LoadingIcon.jsx
+++ webpage/Frontend/src/components/LoadingIcon/LoadingIcon.jsx
@@ -5,7 +5,7 @@ import loadingIcon from '../../assets/Book.webm';
 
 function LoadingIcon() {
   return (
-    <div class="loading-book">
+    <div className="loading-book">
         <video className ="loading-video" width="300" height="300" autoPlay loop muted playsInline>
             <source src={loadingIcon} type="video/webm" />
       </video>
